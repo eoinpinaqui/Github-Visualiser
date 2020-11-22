f20bcd43c12f49c4ea5e64d8cb150d3887077bda
Eoin Pinaqui <45332521+eoinpinaqui@users.noreply.github.com>
2020-11-22 23:37:49 +0000

    Added search bars and inital user component

--- github-visualiser/src/App.vue
+++ github-visualiser/src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="What is up guys?!"/>
-  </div>
+  <v-app>
+
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-text-field
+            label="Search"
+            v-model="tempSearch"
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-select
+            :items="dropdown"
+            v-model="dropdownSelected"
+            label="Where?"
+          ></v-select>
+          <div style="text-align: right">
+            <v-btn
+                v-on:click="getData"
+                color="primary"
+                elevation="4"
+                large
+            >Let's go!</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-main>
+      <User v-if="displayUser && user_data != 'unknown'" v-bind:userData="user_data" />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import User from './components/User.vue';
+import axios from "axios";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    User
+  },
+
+  data: () => ({
+
+    // variables for searching
+    tempSearch: "",
+    search: "",
+    dropdown: ["Users", "Repositories", "Organisations"],
+    dropdownSelected: "",
+
+    // booleans for conditional rendering
+    displayUser: false,
+    displayRepo: false,
+    displayOrg: false,
+    displayUnknown: false,
+
+    // user variables
+    user_data: ""
+  }),
+
+  methods: {
+    getData: function() {
+      const baseURL = "https://api.github.com";
+
+      this.search = this.tempSearch;
+      this.user_data = "unknown";
+
+      if(this.dropdownSelected === "Users") {
+        const urlToQuery = baseURL + "/users/" + this.search;
+        axios
+            .get(urlToQuery)
+            .then(response => (this.user_data = response))
+            .catch(error => alert(error));
+
+        this.displayUser = true;
+        this.displayRepo = false;
+        this.displayOrg = false;
+        this.displayUnknown = false;
+      } else if(this.dropdownSelected === "Repositories") {
+        this.displayUser = false;
+        this.displayRepo = true;
+        this.displayOrg = false;
+        this.displayUnknown = false;
+      } else if(this.dropdownSelected === "Organisations") {
+        this.displayUser = false;
+        this.displayRepo = false;
+        this.displayOrg = true;
+        this.displayUnknown = false;
+      } else {
+        this.displayUser = false;
+        this.displayRepo = false;
+        this.displayOrg = false;
+        this.displayUnknown = false;
+      }
+    }
+  }
+};
+</script>
---
+++ github-visualiser/src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container>
+      <v-card
+          elevation="2"
+      >
+        <v-card-title>{{userData.data.login}}</v-card-title>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col>
+            <v-img
+              max-width="500px"
+              :src=userData.data.avatar_url
+              style="margin: 0 auto; border-radius: 50%"
+              ></v-img>
+          </v-col>
+          <v-col>
+            <p>{{userData}}</p>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  props: {
+    userData: Object
+  }
+}
+</script>
