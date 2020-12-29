121d47b3f066022d7416dbb0f0b76dfdc5336383
Eoin Pinaqui <45332521+eoinpinaqui@users.noreply.github.com>
2020-12-29 19:32:08 +0000

    Added repository hero metric to repository components

--- github-visualiser/src/components/Repo.vue
+++ github-visualiser/src/components/Repo.vue
@@ -15,6 +15,12 @@
             align="center"
         >{{ repoDescription }}
         </v-card-text>
+        <v-row>
+          <v-col>
+            <h3>Commits by contributors over time:</h3>
+            <line-chart :data="chartData"></line-chart>
+          </v-col>
+        </v-row>
         <v-row
         >
           <v-col>
@@ -37,7 +43,8 @@
               <div style="display: flex; flex-direction: row; align-items: center">
                 <h4 style="margin: 0">{{ index + 1 }}.</h4>
                 <img style="margin: 0 1em;width: 3rem; border-radius: 50%" :src=user.image />
-                <p style="margin: 0"><strong>{{ user.name }}:</strong> {{ user.additions }} additions and {{ user.deletions }} deletions</p>
+                <p style="margin: 0"><strong>{{ user.name }}:</strong> {{ user.additions }} additions and
+                  {{ user.deletions }} deletions</p>
               </div>
             </v-card>
           </v-col>
@@ -56,8 +63,31 @@
         </v-row>
         <v-row>
           <v-col>
-            <h3>Commits by user over time:</h3>
-            <line-chart :data="chartData"></line-chart>
+            <v-card
+                elevation="2"
+                style="padding: 1rem; margin: 0.5rem"
+            >
+              <v-card-title
+                  class="justify-center"
+                  style="font-size: 2rem; padding-top: 1em"
+              >Repository Heroes
+              </v-card-title>
+              <v-card-text
+                  align="center"
+              >"Hero Rating" is a metric (devised by Eoin Pinaqui for the CSU33012 Software Engineering module) that
+                can be used to rank contributors based on their involvement in the repository.
+              </v-card-text>
+              <v-card v-on:click="searchUser(user[0])" v-for="user in shortHeroRating" :key="user"
+                      elevation="2"
+                      class="justify-center"
+                      style="padding: 1rem; margin: 2em auto; text-align: center; max-width: 500px">
+                <div style="display: flex; flex-direction: row; align-items: center">
+                  <v-icon>mdi-cards-heart</v-icon>
+                  <img style="margin: 0 1em; width: 10em; border-radius: 50%" :src=user[2] />
+                  <p style="margin: 0; font-size: 2em"><strong>{{ user[0] }}</strong></p>
+                </div>
+              </v-card>
+            </v-card>
           </v-col>
         </v-row>
         <v-row style="margin: 1em">
@@ -66,7 +96,7 @@
             <div style="display: flex; flex-direction: row; align-items: center; flex-wrap: wrap">
               <v-card v-on:click="searchUser(user.login)" v-for="user in allContributors" :key="user"
                       elevation="2"
-                      style="padding: 1rem; margin: 0.5rem; width: 19%">
+                      style="padding: 1rem; margin: 0.5rem; width: 19%; text-align: center">
                 <div style="display: flex; flex-direction: row; align-items: center">
                   <img style="margin: 0 1em;width: 3rem; border-radius: 50%" :src=user.image />
                   <p style="margin: 0"><strong>{{ user.login }}</strong></p>
@@ -107,6 +137,8 @@ export default {
     additionsDeletions: [],
     commenters: [],
     allCommenters: [],
+    heroRating: [],
+    shortHeroRating: [],
   }),
 
   props: {
@@ -143,6 +175,7 @@ export default {
       this.additionsDeletions = [];
       this.commenters = [];
       this.allCommenters = [];
+      this.heroRating = [];
       this.getRepoInfo();
     },
 
@@ -218,6 +251,7 @@ export default {
           .then(response => {
             let contributorsInfo = response.data;
             contributorsInfo.reverse();
+            contributorsInfo = contributorsInfo.slice(0, 25);
 
             this.chartData = [];
             for (let i = 0; i < contributorsInfo.length; i++) {
@@ -246,6 +280,7 @@ export default {
             }
             this.allAdditionsDeletions.sort((a, b) => (a.additions + a.deletions > b.additions + b.deletions) ? -1 : (b.additions + b.deletions > a.additions + a.deletions) ? 1 : 0);
             this.additionsDeletions = this.allAdditionsDeletions.slice(0, 5);
+            this.calculateHeroRatings();
           })
           .catch(error => {
             this.display = false;
@@ -290,6 +325,66 @@ export default {
             this.display = false;
             console.log(error);
           })
+    },
+
+    calculateHeroRatings() {
+
+      setTimeout(function() {
+
+      }, 2000);
+      let commits = this.allContributors;
+      let additions = this.allAdditionsDeletions;
+      let commenters = this.allCommenters;
+
+      let totalCommits = 0;
+      let totalAdditions = 0;
+      let totalComments = 0;
+
+
+      for (let i = 0; i < commits.length; i++) {
+        totalCommits += commits[i].num;
+      }
+
+
+      for (let i = 0; i < additions.length; i++) {
+        totalAdditions += additions[i].additions + additions[i].deletions;
+      }
+
+      for(let i = 0; i < commenters.length; i++) {
+        totalComments += commenters[i].data.num;
+      }
+
+
+      let heroMap = new Map();
+
+      for(let i = 0; i < commits.length; i++) {
+        heroMap.set(commits[i].login, [5 * (commits[i].num / totalCommits), commits[i].image]);
+      }
+
+      for(let i = 0; i < additions.length; i++) {
+        if(heroMap.has(additions[i].name)) {
+          let x = heroMap.get(additions[i].name);
+          let y = 4 * ((additions[i].additions + additions[i].deletions) / totalAdditions);
+          let z = [y + x[0], x[1]];
+          heroMap.set(additions[i].name, z);
+        }
+      }
+
+      for(let i = 0; i < commenters.length; i++) {
+        if(heroMap.has(commenters[i].login)) {
+          let x = heroMap.get(commenters[i].login);
+          let y = (commenters[i].data.num) / totalComments;
+          let z = [y + x[0], x[1]];
+          heroMap.set(commenters[i].login, z);
+        }
+      }
+
+      for(const [key, value] of heroMap) {
+        this.heroRating.push([key, value[0], value[1]]);
+      }
+
+      this.heroRating.sort((a, b) => (a[1] > b[1]) ? -1 : (b[1] > a[1]) ? 1 : 0);
+      this.shortHeroRating = this.heroRating.slice(0, 3);
     },
 
     searchUser(user) {
